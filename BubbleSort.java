e1dbc0230a266db1927e474c8b4bdb28322faa22
Jahir Abbas Sk <jahirabbas676@gmail.com>
2021-08-09 23:40:18 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		int a[]= {6, 1, -3, 8, 5, 4};
+		int n= a.length;
+		
+		for (int i=0; i<n-1; i++) {
+			boolean sorted= true;
+			for (int j=0; j<n-1-i; j++) {
+			
+				if (a[j+1] < a[j]) {
+					
+					int temp= a[j+1];
+					a[j+1]=a[j];
+					a[j]=temp;
+					sorted=false;
+				}
+			}
+			
+			if (sorted) break;
+
+	}
+		for (int element: a) {
+			System.out.print(element+ " ");
+		}
+	}
+
+}
